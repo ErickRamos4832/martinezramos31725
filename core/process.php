4914e3ca762abb9980ee1073eb21fed1e6a16acf
ErickRamos4832 <ia.erickramos@ufg.edu.sv>
2025-07-31 19:54:38 -0600

    practica 31 7

---
+++ core/process.php
@@ -0,0 +1,93 @@
+<?php
+
+$host_aceptadows = array('localhost','127.0.0.1');
+$metodo_aceptado = 'POST';
+$usuario_correcto = "Admin";
+$password_correcto = "Admin";
+
+$txt_usuario = $_POST["txt_usuario"];
+$txt_password = $_POST["txt_password"];
+$token = "";
+
+if( in_array($_SERVER["HTTP_HOST"],$host_aceptados) ){
+    //La direccion IP es aceptada
+    if($_SERVER["REQUEST_METHOD"] == $metodo_aceptado){
+        //Se acepta el metodo usado por el usuario
+        if(isset($txt_usuario) && !empty($txt_usuario)){
+            //Si se enviaron los valoresen el campo usuario
+            if(isset($txt_password) && !empty($txt_password)){
+                //Si envio el valor de la contrasena
+                if($txt_usuario==$usuario_correcto){
+                    //El valor ingresado del campo usuario es correcto
+                    if($txt_password==$password_correcto){
+                    //El valor ingresado del campo password es correcto
+                    $ruta = "welcome.php";
+                    $msg = "";
+                    $codigo_estado = 200;
+                    $texto_estado = "OK";
+                    list($usec,$sec) = esxplode('',microtime());
+                    $token = base64_encode(date("Y-m-d H:i:s",$sec).substr($usec,1));
+                    
+                    } else {
+                        //El valor ingresado del campo password no es correcto
+                        $ruta = "";
+                        $msg = "Su contrasena es incorrecta";
+                        $codigo_estado = 400;
+                        $texto_estado = "Bad Request";
+                        $token = "";
+                    } else {
+                    //El valor del campo usuario no es correcto
+                    $ruta = "";
+                    $msg = "No se reconoce el usuario digitado";
+                    $codigo_estado = 401;
+                    $texto_estado = "Unauthorized";
+                    $token = "";
+                }
+            } else {
+                //El campo password esta vacio
+                $ruta = "";
+                $msg = "EL CAMPO CONTRASENA ESTA VACIO";
+                $codigo_estado = 401;
+                $texto_estado = "Unauthorized";
+                $token = "";
+            }
+        } else {
+            //El campo usuario esta vacio
+            $ruta = "";
+            $msg = "EL CAMPO USUARIO ESTA VACIO";
+            $codigo_estado = 401;
+            $texto_estado = "Unauthorized";
+            $token = "";
+        }
+    } else{
+        //No se acepta el metodo usado por el usuario
+            $ruta = "";
+            $msg = "EL CAMPO USUARIO ESTA VACIO";
+            $codigo_estado = 405;
+            $texto_estado = "Method Not Allowed";
+            $token = "";
+    }
+}else{
+    //La direccion IP no es aceptada
+            $ruta = "";
+            $msg = "SU EQUIPO NO ESTA AUTORIZADO PARA REALIZAR ESTA PETICION";
+            $codigo_estado = 403;
+            $texto_estado = "Forbidden";
+            $token = "";
+}
+
+$arreglo_respuesta = array(
+    "status" => ( (intval($codigo_estado) == 200) ? "ok" : "Error"),
+    "error" => ( (intval($codigo_estado) == 200) ? "" : array("code"=>$codigo_estado,"message"=>$msg)),
+    "data" => array(
+        "url"=$ruta,
+        "token"=>$token
+    ),
+    "count"=>1
+);
+
+header("HTTP/1.1 ".$codigo_estado." ".$texto_estado);
+header("Content-Type: Application/json");
+echo(json_encode($arreglo_respuesta));
+
+?>
